8917eab87c1e144d4a70ffb2dda66f8b19e09422
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-03-09 18:56:12 -0700

    fixed background color

--- iOSMetrix/Controllers/LoginViewController.swift
+++ iOSMetrix/Controllers/LoginViewController.swift
@@ -10,15 +10,6 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    let logo: UILabel = {
-        let logo = UILabel()
-        logo.font = UIFont(name: "HelveticaNeue-UltraLight", size: 75)
-        logo.text = "Metrix"
-        logo.textColor = .black
-        logo.textAlignment = .center
-        logo.translatesAutoresizingMaskIntoConstraints = false
-        return logo
-    }()
     
     let stackView: UIStackView = {
         let stackView = UIStackView()
@@ -29,10 +20,61 @@ class LoginViewController: UIViewController {
         return stackView
     }()
     
+    let loginStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    let logoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    let logo: UILabel = {
+        let logo = UILabel()
+        logo.font = UIFont(name: "HelveticaNeue-UltraLight", size: 75)
+        logo.text = "Metrix"
+        logo.textColor = .white
+        logo.textAlignment = .center
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        return logo
+    }()
+    
+    let missionLogo: UILabel = {
+        let logo = UILabel()
+        logo.font = UIFont(name: "HelveticaNeue-UltraLight", size: 20)
+        logo.text = "Toss the Excel sheet"
+        logo.textColor = .white
+        logo.textAlignment = .center
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        return logo
+    }()
+    
+    let userNameTextField: UITextField = {
+        let field = UITextField()
+        field.attributedPlaceholder = NSAttributedString(string: "Username",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        field.isSecureTextEntry = false
+        field.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+        field.textColor = .white
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.layer.cornerRadius = 10
+        field.textAlignment = .center
+        return field
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = UIColor(red:0.09, green:0.09, blue:0.11, alpha:1.0)
         setViews()
     }
     
@@ -41,13 +83,32 @@ class LoginViewController: UIViewController {
     }
     
     func setViews(){
-        self.view.addSubview(logo)
         
-        self.logo.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        self.logo.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        self.logo.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        self.logo.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-
+        
+        //parent stack
+        self.view.addSubview(stackView)
+        stackView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.80).isActive = true
+        stackView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
+        //logo stack
+        self.stackView.addArrangedSubview(logoStackView)
+        logoStackView.heightAnchor.constraint(equalToConstant: 90.0).isActive = true
+        logoStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        logoStackView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        logoStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        logoStackView.addArrangedSubview(logo)
+        logo.heightAnchor.constraint(equalTo: logoStackView.heightAnchor, multiplier: 0.70).isActive = true
+        logoStackView.addArrangedSubview(missionLogo)
+        
+        
+        //login stack
+        stackView.addArrangedSubview(loginStackView)
+//        loginStackView.widthAnchor.constraint(equalTo: self.stackView.widthAnchor, multiplier: 0.50).isActive = true
+        loginStackView.centerXAnchor.constraint(equalTo: self.stackView.centerXAnchor).isActive = true
+        loginStackView.addArrangedSubview(userNameTextField)
+        userNameTextField.widthAnchor.constraint(equalTo: self.loginStackView.widthAnchor, multiplier: 0.50).isActive = true
+        userNameTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
     }
 
 
